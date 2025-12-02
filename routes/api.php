9644fb82bf04afa9b347058957abbc24561059dd
ahmed11711 <ahmedsamir11711@gmail.com>
2025-12-02 20:24:24 +0200

    aa

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,13 @@ use App\Http\Middleware\CheckJwtToken;
 use App\Http\Controllers\Api\Auth\AuthController;
 
 Route::prefix('v1/')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
+  Route::prefix('auth/')->group(function () {
+
+      Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
     Route::post('verify-email', [AuthController::class, 'verifyEmail']);
     Route::post('verify-affiliate', [AuthController::class, 'verifyAffiliate']);
+    });
     // Route::post('resend-otp', [AuthController::class, 'resendOtp']);
    
 
