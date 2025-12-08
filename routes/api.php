90e12f217b4704fb725af66172069ec7b07c3cce
ahmed11711 <ahmedsamir11711@gmail.com>
2025-12-08 23:47:59 +0200

    aa

--- routes/api.php
+++ routes/api.php
@@ -2,40 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckJwtToken;
-use App\Http\Controllers\Api\Auth\AuthController;
-use App\Http\Controllers\Api\Company\CompanyCOntroller;
-use App\Http\Controllers\Api\Withdraw\WithdrawController;
-use App\Http\Controllers\Api\Affiliate\AffiliateController;
-use App\Http\Controllers\Api\Notifications\NotificationsController;
+use Illuminate\Support\Facades\Artisan;
+
 
 Route::prefix('v1/')->group(function () {
-  Route::prefix('auth/')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('verify-email', [AuthController::class, 'verifyEmail']);
-    Route::post('verify-affiliate', [AuthController::class, 'verifyAffiliate']);
-    Route::post('resend-otp', [AuthController::class, 'resendOtp']);
-  
-    Route::middleware(CheckJwtToken::class)->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('me', [AuthController::class, 'me']);
-    Route::post('update-profile', [AuthController::class, 'updateProfile']);
 
-    });
-   });
+  Route::get('run-migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
 
-       Route::middleware(CheckJwtToken::class)->group(function () {
-                Route::get('withdraw', [WithdrawController::class, 'index']);
-
-       // without auth routes
-        Route::get('my-affiliate', [AffiliateController::class, 'myAffiliate']);
-        Route::get('notification',[NotificationsController::class,'index']);
-
-        Route::post('withdraw', [WithdrawController::class, 'Withdraw']);
-        Route::post('add-balance', [WithdrawController::class, 'addBalance']);
-        Route::get('companies',[CompanyCOntroller::class,'index']);
-        Route::get('settings',[CompanyCOntroller::class,'setting']);
-   });
+    return response()->json([
+      'code' => Artisan::output()
+    ]);
+  });
 });
 
-require __DIR__.'/admin.php';
+require __DIR__ . '/admin.php';
