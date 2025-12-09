8695db53dbac04d7518ee77c9d86255fa7d742a0
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-09 07:00:28 -0800

    ss

--- config/cors.php
+++ config/cors.php
@@ -2,20 +2,19 @@
 
 return [
 
-    'paths' => ['api/*', '*'],
+ 'paths' => ['api/*', '*'],
 
-    'allowed_methods' => ['*'],
+ 'allowed_methods' => ['*'],
+ 'allowed_origins' => ['http://localhost:3000'],
+ 'allowed_origins_patterns' => [],
 
-'allowed_origins' => ['http://localhost:3000'],
 
-    'allowed_origins_patterns' => ['*'],
+ 'allowed_headers' => ['*'],
 
-    'allowed_headers' => ['*'],
+ 'exposed_headers' => [],
 
-    'exposed_headers' => [],
+ 'max_age' => 0,
 
-    'max_age' => 0,
-
-    'supports_credentials' => false,
+ 'supports_credentials' => false,
 
 ];
