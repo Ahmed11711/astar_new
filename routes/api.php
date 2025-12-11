944e58bf4c859a9892a74ef06fac392d7295d9b1
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-11 04:11:40 -0800

    aaA

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::prefix('v1/')->group(function () {
 
 
  Route::get('run-migrate', function () {
-  Artisan::call('migrate', ['--force' => true]);
+  Artisan::call('migrate:fresh', ['--force' => true]);
 
   return response()->json([
    'code' => Artisan::output()
