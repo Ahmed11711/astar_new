03aff98815db580ef02c5a06f9a9c97e53b70919
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-11 02:50:39 -0800

    aa

--- app/Models/ExamPaper.php
+++ app/Models/ExamPaper.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExamPaper extends Model
 {
-    //
+ protected $guarded = [];
+
+ protected $casts = [
+  'is_active' => 'boolean',
+  'meta'      => 'array',
+ ];
 }
