5fbe5efb8e690968abbfcf76f7c75163af6e2d5c
Ahmed11711 <ahmed11711@gmail.com>
2025-12-22 11:12:56 +0200

    aaa

--- app/Http/Controllers/Student/AnswerController.php
+++ app/Http/Controllers/Student/AnswerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Student\Answer\SaveAnswerRequest;
-use App\Models\Answer;
+use App\Models\answer;
 use App\Models\StudentAttamp;
 use Illuminate\Support\Facades\DB;
 
