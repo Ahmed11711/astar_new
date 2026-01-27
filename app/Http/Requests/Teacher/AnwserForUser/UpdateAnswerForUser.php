8e2d4c8d9983deb18c9177b130b436e6d9a820bf
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-01-27 11:49:56 +0200

    user add

--- app/Http/Requests/Teacher/AnwserForUser/UpdateAnswerForUser.php
+++ app/Http/Requests/Teacher/AnwserForUser/UpdateAnswerForUser.php
@@ -14,7 +14,7 @@ class UpdateAnswerForUser extends BaseRequest
             'is_correct'      => 'required|boolean',
             'mark_score'   => 'required|numeric|min:0',
             'teacher_feedback' => 'required|string',
-            'student_id'      => 'sometimes|integer|exists:users,id',
+            'student_id'      => 'required|integer|exists:users,id',
         ];
     }
 }
