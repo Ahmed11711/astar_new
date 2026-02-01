15f0c550eb9c0f402f097644644de80aa8c1ad0f
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-02-01 14:22:22 +0200

    44

--- app/Models/UserPackageFeature.php
+++ app/Models/UserPackageFeature.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserPackageFeature extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function package()
+    {
+        return $this->belongsTo(Packages::class);
+    }
+
+    public function feature()
+    {
+        return $this->belongsTo(FeaturePackage::class, 'package_feature_id');
+    }
 }
