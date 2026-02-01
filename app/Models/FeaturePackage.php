de491c0e9a06d70577470938046921f8d2475d49
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-02-01 16:01:51 +0200

    keys

--- app/Models/FeaturePackage.php
+++ app/Models/FeaturePackage.php
@@ -3,19 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class FeaturePackage extends Model
 {
- //
+    //
 
- public function package()
- {
-  return $this->belongsTo(Packages::class, 'package_id');
- }
+    public function package()
+    {
+        return $this->belongsTo(Packages::class, 'package_id');
+    }
 
 
- public function feature()
- {
-  return $this->belongsTo(Feature::class, 'feature_id');
- }
+    public function feature()
+    {
+        return $this->belongsTo(Feature::class, 'feature_id');
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            if ($model->feature) {
+                // توليد key_feature من اسم الـ Feature
+                $model->key_feature = Str::snake($model->feature->key);
+            }
+        });
+    }
 }
