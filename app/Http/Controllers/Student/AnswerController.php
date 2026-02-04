753c60d35e42738c468adecae093dc5f56ba50b0
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-02-04 14:34:38 +0200

    ff

--- app/Http/Controllers/Student/AnswerController.php
+++ app/Http/Controllers/Student/AnswerController.php
@@ -14,139 +14,139 @@ use Symfony\Component\HttpFoundation\Request;
 
 class AnswerController extends Controller
 {
-    // public function saveAnswersOptimized(SaveAnswerRequest $request)
-    // {
-    //     Log::alert('saveAnswersOptimized called', $request->all());
-    //     $userId    = $request->user_id;
-    //     $attemptId = $request->attempt_id;
-    //     $is_saved  = $request->is_saved;
+    public function saveAnswersOptimized(SaveAnswerRequest $request)
+    {
+        Log::alert('saveAnswersOptimized called', $request->all());
+        $userId    = $request->user_id;
+        $attemptId = $request->attempt_id;
+        $is_saved  = $request->is_saved;
 
-    //     // Check attempt ownership
-    //     $attempt = StudentAttamp::where('id', $attemptId)
-    //         ->where('user_id', $userId)
-    //         ->first();
-
-
-
-    //     if (! $attempt) {
-    //         return response()->json([
-    //             'message' => 'Attempt not found or does not belong to this user'
-    //         ], 404);
-    //     }
-
-    //     // Answers data & files
-    //     $answersData  = $request->input('answers', []);
-    //     $answersFiles = $request->files->get('answers', []);
-
-    //     //  File paths configuration
-    //     $paths = [
-    //         'drawing_answer' => [
-    //             'folder' => public_path('storage/answers/drawings'),
-    //             'url'    => 'public/storage/answers/drawings',
-    //             'prefix' => 'draw_',
-    //         ],
-    //         'audio_answer' => [
-    //             'folder' => public_path('storage/answers/audio'),
-    //             'url'    => 'public/storage/answers/audio',
-    //             'prefix' => 'audio_',
-    //         ],
-    //     ];
-
-    //     //  Ensure folders exist
-    //     foreach ($paths as $config) {
-    //         if (! file_exists($config['folder'])) {
-    //             mkdir($config['folder'], 0755, true);
-    //         }
-    //     }
-
-    //     $upsertData = [];
+        // Check attempt ownership
+        $attempt = StudentAttamp::where('id', $attemptId)
+            ->where('user_id', $userId)
+            ->first();
 
 
-    //     $operationTime = now();
 
-    //     foreach ($answersData as $index => $answer) {
+        if (! $attempt) {
+            return response()->json([
+                'message' => 'Attempt not found or does not belong to this user'
+            ], 404);
+        }
 
-    //         $response = $answer['response'] ?? [];
+        // Answers data & files
+        $answersData  = $request->input('answers', []);
+        $answersFiles = $request->files->get('answers', []);
 
-    //         foreach ($paths as $key => $config) {
+        //  File paths configuration
+        $paths = [
+            'drawing_answer' => [
+                'folder' => public_path('storage/answers/drawings'),
+                'url'    => 'public/storage/answers/drawings',
+                'prefix' => 'draw_',
+            ],
+            'audio_answer' => [
+                'folder' => public_path('storage/answers/audio'),
+                'url'    => 'public/storage/answers/audio',
+                'prefix' => 'audio_',
+            ],
+        ];
 
-    //             if (
-    //                 isset($answersFiles[$index]['response'][$key]) &&
-    //                 $answersFiles[$index]['response'][$key]->isValid()
-    //             ) {
-    //                 $file = $answersFiles[$index]['response'][$key];
-    //                 $ext  = $file->getClientOriginalExtension();
+        //  Ensure folders exist
+        foreach ($paths as $config) {
+            if (! file_exists($config['folder'])) {
+                mkdir($config['folder'], 0755, true);
+            }
+        }
 
-    //                 $fileName = uniqid($config['prefix']) . '.' . $ext;
-
-    //                 // Move file
-    //                 $file->move($config['folder'], $fileName);
-
-    //                 // Save correct URL in response
-    //                 $response[$key] = url($config['url'] . '/' . $fileName);
-    //             }
-    //         }
-
-    //         $upsertData[] = [
-    //             'attempt_id'     => $attemptId,
-    //             'user_id'        => $userId,
-    //             'question_id'    => $answer['question_id'],
-    //             'question_index' => $answer['question_index'],
-    //             'response'       => json_encode($response, JSON_UNESCAPED_UNICODE),
-    //             'is_flagged'     => $answer['is_flagged'] ?? false,
-    //             'created_at'     => $operationTime,
-    //             'updated_at'     => $operationTime,
-    //         ];
-    //         Log::info('Prepared answer data for upsert', $upsertData);
-    //     }
-
-    //     $answerIds = [];
-
-    //     DB::transaction(function () use (
-    //         $upsertData,
-    //         $attemptId,
-    //         $userId,
-    //         $operationTime,
-    //         &$answerIds
-    //     ) {
-
-    //         // Save answers
-    //         answer::upsert(
-    //             $upsertData,
-    //             ['attempt_id', 'question_id', 'question_index', 'user_id'],
-    //             ['response', 'is_flagged', 'updated_at']
-    //         );
-
-    //         // Mark attempt as saved
-    //         StudentAttamp::where('id', $attemptId)
-    //             ->update(['is_saved' => true]);
-
-    //         //  ONLY answers touched in THIS request
-    //         $answerIds = answer::where('attempt_id', $attemptId)
-    //             ->where('user_id', $userId)
-    //             ->where('updated_at', $operationTime)
-    //             ->pluck('id')
-    //             ->toArray();
-    //     });
-
-    //     //  Send to AI only when final save
-    //     if ($is_saved && ! empty($answerIds)) {
+        $upsertData = [];
 
 
-    //         Http::withHeaders([
-    //             'Content-Type' => 'application/json',
-    //         ])->post('https://ai.astar.click/get_marks', [
-    //             'answer_ids' => $answerIds,
-    //         ]);
-    //     }
+        $operationTime = now();
 
-    //     return response()->json([
-    //         'message'    => 'All answers saved successfully.',
-    //         'answer_ids' => $answerIds,
-    //     ]);
-    // }
+        foreach ($answersData as $index => $answer) {
 
-    public function saveAnswersOptimized(SaveAnswerRequest $request, AnswerService $service)
+            $response = $answer['response'] ?? [];
+
+            foreach ($paths as $key => $config) {
+
+                if (
+                    isset($answersFiles[$index]['response'][$key]) &&
+                    $answersFiles[$index]['response'][$key]->isValid()
+                ) {
+                    $file = $answersFiles[$index]['response'][$key];
+                    $ext  = $file->getClientOriginalExtension();
+
+                    $fileName = uniqid($config['prefix']) . '.' . $ext;
+
+                    // Move file
+                    $file->move($config['folder'], $fileName);
+
+                    // Save correct URL in response
+                    $response[$key] = url($config['url'] . '/' . $fileName);
+                }
+            }
+
+            $upsertData[] = [
+                'attempt_id'     => $attemptId,
+                'user_id'        => $userId,
+                'question_id'    => $answer['question_id'],
+                'question_index' => $answer['question_index'],
+                'response'       => json_encode($response, JSON_UNESCAPED_UNICODE),
+                'is_flagged'     => $answer['is_flagged'] ?? false,
+                'created_at'     => $operationTime,
+                'updated_at'     => $operationTime,
+            ];
+            Log::info('Prepared answer data for upsert', $upsertData);
+        }
+
+        $answerIds = [];
+
+        DB::transaction(function () use (
+            $upsertData,
+            $attemptId,
+            $userId,
+            $operationTime,
+            &$answerIds
+        ) {
+
+            // Save answers
+            answer::upsert(
+                $upsertData,
+                ['attempt_id', 'question_id', 'question_index', 'user_id'],
+                ['response', 'is_flagged', 'updated_at']
+            );
+
+            // Mark attempt as saved
+            StudentAttamp::where('id', $attemptId)
+                ->update(['is_saved' => true]);
+
+            //  ONLY answers touched in THIS request
+            $answerIds = answer::where('attempt_id', $attemptId)
+                ->where('user_id', $userId)
+                ->where('updated_at', $operationTime)
+                ->pluck('id')
+                ->toArray();
+        });
+
+        //  Send to AI only when final save
+        if ($is_saved && ! empty($answerIds)) {
+
+
+            Http::withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post('https://ai.astar.click/get_marks', [
+                'answer_ids' => $answerIds,
+            ]);
+        }
+
+        return response()->json([
+            'message'    => 'All answers saved successfully.',
+            'answer_ids' => $answerIds,
+        ]);
+    }
+
+    public function saveAnswersOptimizeds(SaveAnswerRequest $request, AnswerService $service)
     {
         $validated = $request->validated();
 
