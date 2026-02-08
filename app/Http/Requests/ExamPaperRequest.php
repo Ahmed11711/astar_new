a783854c816d64cd57972f402ae6ebb6d190f83e
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-02-08 16:40:54 +0200

    create-exame-without-paper_id

--- app/Http/Requests/ExamPaperRequest.php
+++ app/Http/Requests/ExamPaperRequest.php
@@ -61,7 +61,7 @@ class ExamPaperRequest extends BaseRequest
             'grade_id'          => 'required|exists:grades,id',
             'title'             => 'required|string|max:255',
             'paper_label'       => 'nullable|string|max:255',
-            'paper_id'          => 'required|exists:papers,id',
+            'paper_id'          => 'nullable|exists:papers,id',
             'year'              => 'nullable|integer',
             'month'             => 'nullable|string',
             'total_marks'       => 'nullable|integer',
