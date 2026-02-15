e95f3ea909ce4bfc88146121a7f3e7bb5ae0b53f
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-02-15 12:13:18 +0200

    rest-password

--- app/Http/Controllers/Auth/ForgetPassword/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ForgetPassword/ResetPasswordController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\RestPasswordRequest;
 use App\Models\User;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
@@ -19,24 +20,30 @@ class ResetPasswordController extends Controller
     {
         $request->validated();
 
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user, $password) {
-                $user->password = Hash::make($password);
-                $user->save();
-            }
-        );
+        $token = $request->token;
+        $email = $request->email;
 
-        if ($status === Password::PASSWORD_RESET) {
-            return response()->json([
-                'message' => 'Password reset successfully!',
-                'status' => 'success',
-            ]);
-        } else {
-            return response()->json([
-                'message' => 'Invalid or expired token',
-                'status' => 'error',
-            ]);
+        $tokenData = DB::table('password_reset_tokens')
+            ->where('email', $email)
+            ->first();
+
+        if (!$tokenData || Carbon::parse($tokenData->created_at)->addMinutes(5)->isPast()) {
+            return $this->errorResponse('Invalid or expired token');
         }
+
+        if (!Hash::check($token, $tokenData->token)) {
+            return $this->errorResponse('Invalid token');
+        }
+
+        $user = User::where('email', $email)->first();
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        DB::table('password_reset_tokens')->where('email', $email)->delete();
+
+        return response()->json([
+            'message' => 'Password reset successfully!',
+            'status' => 'success',
+        ]);
     }
 }
