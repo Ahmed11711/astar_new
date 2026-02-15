6708810ff45a1b4384d58e5e6efbe0a27180b3ef
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-02-15 13:47:38 +0200

    ff

--- app/Http/Resources/Admin/User/UserResource.php
+++ app/Http/Resources/Admin/User/UserResource.php
@@ -15,7 +15,7 @@ class UserResource extends JsonResource
             'last_name' => $this->last_name,
             'email' => $this->email,
             'password' => $this->password,
-            'grade' => optional($this->grades->first())->id,
+            'grade' => optional($this->grades->first())->name,
 
             'phone' => $this->phone,
             'is_email_verified' => $this->is_email_verified,
